b63a20dd70c129c7a4ac938d97037382d3f12174
Zenbeau <merlin.r200@gmail.com>
2024-09-13 23:44:35 -0400

    added Header, Nav, Main, Footer, images, meta tags

---
+++ my-react-app/src/Header.js
@@ -0,0 +1,14 @@
+import logo from './logo.png';
+
+function Header() {
+    return (
+        <img
+	    height={50}
+        width={150}
+	    src={logo}
+	    alt="Little Lemon logo"
+	    />
+    )
+  }
+
+  export default Header;
---
+++ my-react-app/src/Main.js
@@ -0,0 +1,10 @@
+function Main() {
+    return (
+      <main>
+        <h1>Welcome to Little Lemon</h1>
+        <p>This is where the main content of the page goes.</p>
+      </main>
+    );
+  }
+
+  export default Main;
